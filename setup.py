6dd1c17ad7f9866a14fd4b127507fe44495468b6
info@vltmedia.com <info@vltmedia.com>
2024-04-11 01:19:49 -0400

    Feature: Added pypi module upload

--- setup.py
+++ setup.py
@@ -7,4 +7,4 @@ from setuptools import setup, find_packages
 # Setup file for uaiDiffusers.py that requires socket and MediaItems.py and cv2
 #
 # setup(name='uaiDiffusers', version='1.1', py_modules=['uaiDiffusers'],long_description=open('README.md').read(), requires=['socket', 'MediaItems', 'cv2'])
-setup()
+setup(name='uaiDiffusers', version='1.1.2',long_description=open('README.md').read())
